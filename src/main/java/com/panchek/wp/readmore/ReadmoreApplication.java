7bc4aa84accd6a3de9b0111535082cbba846afe2
Панче Котев <Panche.Kotev@students.finki.ukim.mk>
2020-02-12 18:54:38 +0100

    Initial commit

---
+++ src/main/java/com/panchek/wp/readmore/ReadmoreApplication.java
@@ -0,0 +1,13 @@
+package com.panchek.wp.readmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadmoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadmoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/panchek/wp/readmore/ReadmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panchek.wp.readmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadmoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
